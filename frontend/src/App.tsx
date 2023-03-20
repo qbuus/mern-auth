5bb964a3eb1f05da6f1c901f86fb49082bc9afda
qbuus <jkocz867@gmail.com>
2023-03-20 12:15:58 +0100

    React-router-dom setup, default reset styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+
 function App() {
-  return <div>123</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div className="pages">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
