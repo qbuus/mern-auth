97f12c997c3e995bb06ab27304c2e7b2fd52ff16
qbuus <jkocz867@gmail.com>
2023-03-21 17:09:27 +0100

    Data fetching rework

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,27 +1,41 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { Workout } from "../models/Workout";
 import WorkoutDetails from "./components/WorkoutDetails";
 import WorkoutForm from "./components/WorkoutForm";
 
 const Home = () => {
-  const [workouts, setWorkouts] = useState<Workout[] | null>(
-    null
-  );
+  const [workouts, setWorkouts] = useState<Workout[]>([]);
+  const effectRan = useRef<boolean>(false);
+
+  const getUrl = () => {
+    return "/api/workouts";
+  };
 
   useEffect(() => {
+    let subscribed = false;
+
     const fetchData = async () => {
       try {
-        const response = await fetch("/api/workouts");
+        const response = await fetch(getUrl());
         const data = await response.json();
 
-        if (response.ok) {
+        if (!subscribed) {
           setWorkouts(data);
+          console.log(data);
         }
       } catch (error) {
         console.error(error);
       }
     };
-    fetchData();
+
+    if (effectRan.current === false) {
+      fetchData();
+    }
+
+    return () => {
+      subscribed = true;
+      effectRan.current = true;
+    };
   }, []);
 
   return (
