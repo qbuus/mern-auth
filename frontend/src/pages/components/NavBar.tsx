3d3b13fc4b2e92bac724eb8bb1270d9111a8a02a
qbuus <jkocz867@gmail.com>
2023-04-06 10:10:10 +0200

    logout button styles + logout func

--- frontend/src/pages/components/NavBar.tsx
+++ frontend/src/pages/components/NavBar.tsx
@@ -1,6 +1,13 @@
 import { Link } from "react-router-dom";
+import Logout from "../../hooks/Logout";
 
 const NavBar = () => {
+  const { loggingout } = Logout();
+
+  const handleClick = () => {
+    loggingout();
+  };
+
   return (
     <header>
       <div className="container">
@@ -8,6 +15,9 @@ const NavBar = () => {
           <h2>Home</h2>
         </Link>
         <nav>
+          <div>
+            <button onClick={handleClick}>Logout</button>
+          </div>
           <div>
             <Link to="/login">Login</Link>
             <Link to="/signup">signup</Link>
